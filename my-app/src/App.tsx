0203159c99ba9e492157900b5ed642b4b0be4cd8
Rex Oliver <rex.oliver@capgemini.com>
2023-03-03 14:43:57 -0500

    got ahold of grid

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -27,39 +27,55 @@ export default function CalcWindow(){
   
   return (
     <React.Fragment>
-      <FlexItem></FlexItem>
-      <div className="topbox">
-        <ResultBar result={15}/>
-      </div>
-      <div className="board-row">
-        <SymbolNum snum={"AC"}/>
-        <SymbolNum snum={"+/-"}/>
-        <SymbolNum snum={"%"}/>
-        <SymbolNum snum={"/"}/>
-      </div>
-      <div className="board-row">
-        <SymbolNum snum={"7"}/>
-        <SymbolNum snum={"8 "}/>
-        <SymbolNum snum={"9"}/>
-        <SymbolNum snum={"*"}/>
-      </div>
-      <div className="board-row">
-        <SymbolNum snum={"4"}/>
-        <SymbolNum snum={"5"}/>
-        <SymbolNum snum={"6"}/>
-        <SymbolNum snum={"-"}/>
-      </div>
-      <div className="board-row">
-        <SymbolNum snum={"1"}/>
-        <SymbolNum snum={"2"}/>
-        <SymbolNum snum={"3"}/>
-        <SymbolNum snum={"+"}/>
-      </div>
-      <div className="board-row">
-        <SymbolNum snum={"0"}/>
-        <SymbolNum snum={"0"}/>
-        <SymbolNum snum={"."}/>
-        <SymbolNum snum={"="}/>
+      <div className='gridcontainer'>
+        <div className="topbox">
+          <ResultBar result={15}/>
+        </div>
+        <div className='numpad'>
+          <div className='row'>
+            <SymbolNum snum={"7"}/>
+            <SymbolNum snum={"8 "}/>
+            <SymbolNum snum={"9"}/>
+          </div>
+          <div className='row'>
+            <SymbolNum snum={"4"}/>
+            <SymbolNum snum={"5"}/>
+            <SymbolNum snum={"6"}/>
+          </div>
+          <div className='row'>
+            <SymbolNum snum={"1"}/>
+            <SymbolNum snum={"2"}/>
+            <SymbolNum snum={"3"}/>
+          </div>  
+          <div className='row'>
+            <SymbolNum snum={"0"}/>
+            <SymbolNum snum={"0"}/>
+            <SymbolNum snum={"."}/>
+          </div>
+        </div>
+        
+        <div className="board-row">
+          <SymbolNum snum={"AC"}/>
+          <SymbolNum snum={"+/-"}/>
+          <SymbolNum snum={"%"}/>
+          <SymbolNum snum={"/"}/>
+        </div>
+        <div className="board-row">
+          
+          <SymbolNum snum={"*"}/>
+        </div>
+        <div className="board-row">
+          
+          <SymbolNum snum={"-"}/>
+        </div>
+        <div className="board-row">
+          
+          <SymbolNum snum={"+"}/>
+        </div>
+        <div className="board-row">
+          
+          <SymbolNum snum={"="}/>
+        </div>
       </div>
     </React.Fragment>
   );
@@ -69,21 +85,31 @@ const SymbolNum = (props: {snum: string}) => {
   return (
     <h1 className='square'>{props.snum}</h1>
   );
-
 }
 
-const FlexItem = () => {
-  return (
-    <div className="container" id="container">
-      <div>One</div>
-      <div>Item two</div>
-      <div>The item we will refer to as three</div>
-    </div>
-  );
-}
 
 const ResultBar = (props: {result: number}) => {
   return (
     <h1>{props.result}</h1>
   );
 }
+
+
+const GridItem = () => {
+  return (
+    <div className='gridcontainer' id='gridcontainer'>
+      <div className='box'>Texttime</div>
+    </div>
+  );
+}
+
+const FlexItem = () => {
+  return (
+    <div className="container" id="container">
+      <div className='box'>One</div>
+      <div className='box'>Item two</div>
+      <div className='box'>The item we will refer to as three</div>
+    </div>
+  );
+}
+
